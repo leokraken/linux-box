ea516ce9a776824476ad2549bdf2a42b3bc11eca
leokraken <joleocl@gmail.com>
2015-03-01 19:31:31 -0200

    Openbox config files

---
+++ openboxconfig.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#dmenu -> suckless-tools
+
+#install applications
+#sudo apt-get install tint2 conky suckless-tools obmenu nitrogen gtk-chtheme git
+
+#move config files 
+cp rc.xml ~/.config/openbox/rc.xml
+cp menu.xml ~/.config/openbox/menu.xml
+cp autostart ~/.config/openbox/autostart
+cp .conkyrc ../.conkyrc
